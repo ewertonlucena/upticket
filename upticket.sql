640b1192732f2696c92ca9d70bbf9c62903ee640
NOC 2 <NOC 2@DESKTOP-HR9FIKA>
2018-06-12 18:15:13 -0300

    atualização 2 Banco de Dados

--- upticket.sql
+++ upticket.sql
@@ -2,7 +2,7 @@
 -- Servidor:                     127.0.0.1
 -- Versão do servidor:           10.1.32-MariaDB - mariadb.org binary distribution
 -- OS do Servidor:               Win32
--- HeidiSQL Versão:              9.4.0.5125
+-- HeidiSQL Versão:              9.5.0.5196
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -13,12 +13,10 @@
 
 
 -- Copiando estrutura do banco de dados para helpdesk
-DROP DATABASE IF EXISTS `helpdesk`;
 CREATE DATABASE IF NOT EXISTS `helpdesk` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `helpdesk`;
 
 -- Copiando estrutura para tabela helpdesk.departments
-DROP TABLE IF EXISTS `departments`;
 CREATE TABLE IF NOT EXISTS `departments` (
   `id` int(11) NOT NULL,
   `active` tinyint(4) NOT NULL,
@@ -26,6 +24,8 @@ CREATE TABLE IF NOT EXISTS `departments` (
   `email` varchar(100) NOT NULL,
   `groups_allowed` varchar(200) NOT NULL,
   `signature` text NOT NULL,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -34,17 +34,19 @@ CREATE TABLE IF NOT EXISTS `departments` (
 /*!40000 ALTER TABLE `departments` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela helpdesk.help_topics
-DROP TABLE IF EXISTS `help_topics`;
 CREATE TABLE IF NOT EXISTS `help_topics` (
   `id` int(11) NOT NULL,
   `active` tinyint(4) NOT NULL,
   `name` varchar(50) NOT NULL,
+  `description` text NOT NULL,
   `type` varchar(50) NOT NULL,
   `id_parent` int(11) DEFAULT NULL,
   `priority` int(11) NOT NULL,
   `id_department` int(11) NOT NULL,
   `auto_id_staff` int(11) DEFAULT NULL,
   `admin_notes` text,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -53,7 +55,6 @@ CREATE TABLE IF NOT EXISTS `help_topics` (
 /*!40000 ALTER TABLE `help_topics` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela helpdesk.permissions_groups
-DROP TABLE IF EXISTS `permissions_groups`;
 CREATE TABLE IF NOT EXISTS `permissions_groups` (
   `id` int(11) NOT NULL,
   `active` tinyint(4) NOT NULL,
@@ -69,7 +70,6 @@ CREATE TABLE IF NOT EXISTS `permissions_groups` (
 /*!40000 ALTER TABLE `permissions_groups` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela helpdesk.permissions_params
-DROP TABLE IF EXISTS `permissions_params`;
 CREATE TABLE IF NOT EXISTS `permissions_params` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
@@ -82,7 +82,6 @@ CREATE TABLE IF NOT EXISTS `permissions_params` (
 /*!40000 ALTER TABLE `permissions_params` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela helpdesk.staff
-DROP TABLE IF EXISTS `staff`;
 CREATE TABLE IF NOT EXISTS `staff` (
   `id` int(11) NOT NULL,
   `active` tinyint(1) NOT NULL,
@@ -99,6 +98,9 @@ CREATE TABLE IF NOT EXISTS `staff` (
   `vacation` tinyint(1) NOT NULL,
   `teams` varchar(200) DEFAULT NULL,
   `admin_notes` text,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  `last_login` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -107,7 +109,6 @@ CREATE TABLE IF NOT EXISTS `staff` (
 /*!40000 ALTER TABLE `staff` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela helpdesk.stakeholder
-DROP TABLE IF EXISTS `stakeholder`;
 CREATE TABLE IF NOT EXISTS `stakeholder` (
   `id` int(11) NOT NULL,
   `active` tinyint(4) NOT NULL,
@@ -128,6 +129,8 @@ CREATE TABLE IF NOT EXISTS `stakeholder` (
   `addr_ref` varchar(50) DEFAULT NULL,
   `addr_coord` varchar(50) DEFAULT NULL,
   `internal_obs` text,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -136,7 +139,6 @@ CREATE TABLE IF NOT EXISTS `stakeholder` (
 /*!40000 ALTER TABLE `stakeholder` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela helpdesk.stakeholder_contacts
-DROP TABLE IF EXISTS `stakeholder_contacts`;
 CREATE TABLE IF NOT EXISTS `stakeholder_contacts` (
   `id` int(11) NOT NULL,
   `id_stakeholder` int(11) NOT NULL,
@@ -149,16 +151,32 @@ CREATE TABLE IF NOT EXISTS `stakeholder_contacts` (
 /*!40000 ALTER TABLE `stakeholder_contacts` DISABLE KEYS */;
 /*!40000 ALTER TABLE `stakeholder_contacts` ENABLE KEYS */;
 
+-- Copiando estrutura para tabela helpdesk.stakeholder_contracts
+CREATE TABLE IF NOT EXISTS `stakeholder_contracts` (
+  `id` int(11) NOT NULL,
+  `id_stakeholder` int(11) NOT NULL,
+  `active` tinyint(4) NOT NULL,
+  `type` varchar(50) NOT NULL COMMENT 'compra ou venda',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela helpdesk.stakeholder_contracts: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `stakeholder_contracts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stakeholder_contracts` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela helpdesk.stakeholder_users
-DROP TABLE IF EXISTS `stakeholder_users`;
 CREATE TABLE IF NOT EXISTS `stakeholder_users` (
   `id` int(11) NOT NULL,
   `id_stakeholder` int(11) NOT NULL,
+  `active` int(11) NOT NULL,
   `login` varchar(100) NOT NULL,
   `pass` varchar(32) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `phone` varchar(32) NOT NULL,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  `last_login` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -167,7 +185,6 @@ CREATE TABLE IF NOT EXISTS `stakeholder_users` (
 /*!40000 ALTER TABLE `stakeholder_users` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela helpdesk.teams
-DROP TABLE IF EXISTS `teams`;
 CREATE TABLE IF NOT EXISTS `teams` (
   `id` int(11) NOT NULL,
   `active` varchar(50) NOT NULL,
@@ -175,6 +192,8 @@ CREATE TABLE IF NOT EXISTS `teams` (
   `leader_id` int(11) NOT NULL,
   `disable_alerts` tinyint(4) NOT NULL,
   `admin_notes` text,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -183,7 +202,6 @@ CREATE TABLE IF NOT EXISTS `teams` (
 /*!40000 ALTER TABLE `teams` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela helpdesk.tickets
-DROP TABLE IF EXISTS `tickets`;
 CREATE TABLE IF NOT EXISTS `tickets` (
   `id` int(11) NOT NULL,
   `status` varchar(50) NOT NULL,
@@ -193,7 +211,7 @@ CREATE TABLE IF NOT EXISTS `tickets` (
   `priority` tinyint(4) NOT NULL,
   `subject` varchar(100) NOT NULL,
   `content` varchar(100) NOT NULL,
-  `internal_note` varchar(100) DEFAULT NULL,
+  `internal_notes` varchar(100) DEFAULT NULL,
   `create_date` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
