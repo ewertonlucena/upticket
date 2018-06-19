7c911c73cfa78802edc7492d7d13a7f43e74561c
ewert <ewert@10.0.0.103>
2018-06-18 23:39:56 -0300

    Area de Login incompleta

--- views/login.php
+++ views/login.php
@@ -1,49 +1,44 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />        
-        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap.css"/>
-        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/style.css"/>        
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />     
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap.css"/>    
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/style.css"/>                    
         <title>Meu site</title>
     </head>
     <body>
-        <div class="container-fluid vertical-centered">             
-            <div class="row">                
-                <div class="col-md-12">
-                    <?php if(isset($error) && !empty($error)): ?>
-                    <div class="row justify-content-center">                        
-                        <div class="col-md-4">
-                            <div class="alert alert-danger alert-dismissible">			 
-				<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                                <?php echo $error; ?>
-                            </div>
-                        </div>                        
-                    </div>
+        <div class="container-fluid">             
+            <div class="row justify-content-center bg-dark">                    		
+                <div class="col-xs-4 col-sm-9 col-md-7 col-lg-6 col-xl-4 bg-danger">
+                    <?php if (isset($error) && !empty($error)): ?>
+                        <div class="alert alert-danger alert-dismissible mx-4 mb-1">			 
+                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                            <?php echo $error; ?>
+                        </div>
                     <?php endif; ?>
-                    <div class="row justify-content-center">			
-                        <div class="col-md-4 loginarea">
-                            <form method="POST" role="form">
-                                <div class="form-group">
-                                    <label>
-					Login
-                                    </label>
-                                    <input type="text" class="form-control" name="login" placeholder="Digite seu nome de usuário" />
-				</div>
-				<div class="form-group">
-                                    <label>
-					Senha
-                                    </label>
-                                    <input type="password" class="form-control" name="pass" placeholder="Digite sua senha" />
-				</div>	
-                                <div class="form-group">
-                                    <input type="submit" class="btn btn-primary" value="Entrar"/>
-                                </div>
-                            </form>
-			</div>			
+                    <div class="loginarea mt-2">
+                        <form method="POST" role="form">
+                            <div class="form-group">
+                                <label>
+                                    Login
+                                </label>
+                                <input type="text" class="form-control" name="login" placeholder="Digite seu nome de usuário" />
+                            </div>
+                            <div class="form-group">
+                                <label>
+                                    Senha
+                                </label>
+                                <input type="password" class="form-control" name="pass" placeholder="Digite sua senha" />
+                            </div>	
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" value="Entrar"/>
+                            </div>
+                        </form>
                     </div>
-		</div>
+                </div>			
             </div>
         </div>
+
         <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/bootstrap.bundle.min.js"></script>
         <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/script.js"></script>
