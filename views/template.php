1a46a7f86bc1e81fe179fba686ca9f31191c639e
NOC 2 <NOC 2@DESKTOP-HR9FIKA>
2018-06-21 18:10:26 -0300

    Atualização Template

--- views/template.php
+++ views/template.php
@@ -3,23 +3,33 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap.css"/>
-        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/fontawesome-all.min.css"/>
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/all.css"/>
         <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/style.css"/>
         <title>UP Ticket</title>
     </head>
     <body>
         <div class="container-fluid">
             <div id="Header" class="row header">
-                <div id="Logo" class="col-xl-2 col-lg-3 col-md-6 col-sm-6 order-lg-0 order-sm-0 col-12 bg-light sub-header d-flex align-items-center justify-content-sm-start justify-content-center">
-                    <div style="max-height: 60px; width: 60px;">
-                        <img class="img-fluid" src="<?php echo BASE_URL; ?>assets/imgs/logo.jpg">
+                <div id="Logo" class="col-xl-2 col-lg-2 col-md-6 col-sm-6 order-lg-0 order-sm-0 col-12 bg-light sub-header d-flex align-items-center justify-content-sm-start justify-content-center">
+                    <div class="media align-items-end">
+                        <img class="img-fluid mr-0 logo" src="<?php echo BASE_URL; ?>assets/imgs/logo.jpg">
+                        <div class="media-body">
+                            <span class="text-dark logo-text h4"><strong>Tickets</strong></span>
+                        </div>
                     </div>
                 </div>
-                <div id="Navbar" class="col-xl col-lg col-md-12 col-sm-12 order-lg-1 order-sm-2 col-4 bg-light sub-header d-flex align-items-center">
+                <div id="Navbar" class="col-xl col-lg col-md-12 col-sm-12 order-lg-1 order-sm-2 col-3 bg-primary sub-header d-flex align-items-center">
                     NAVBAR
                 </div>
-                <div id="StaffArea" class="col-xl-3 col-lg-3 col-md-6 col-sm-6 order-lg-2 order-sm-1 col bg-light sub-header d-flex align-items-center justify-content-end">
-                    Olá, <?php echo $viewData['staff_name'];?>
+                <div id="StaffArea" class="bg-warning col-xl-4 col-lg-4 col-md-6 col-sm-6 order-lg-2 order-sm-1 col bg-light sub-header d-flex align-items-center justify-content-end">
+                    <div class="btn btn-staff btn-sm rounded-circle d-inline-block mr-2"">
+                        <span class="fas fa-exclamation-triangle"></span>
+                    </div> 
+                    <div class="btn btn-staff btn-sm rounded-circle d-inline-block">
+                        <span class="fas fa-user"></span>
+                    </div> 
+                    <span class="d-none d-sm-inline text-truncate ml-2">Olá, <?php echo $viewData['staff_name']; ?></span>
+                    <a class="btn btn-staff btn-sm border-dark text-nowrap ml-3" href="#">Painel Admin</a>
                 </div>
             </div>
             <div class="row">
@@ -36,13 +46,13 @@
                 </div>
             </div>
             <div class="row bg-info main-content">
-                
+
                 <?php
-                    $this->loadViewInTemplate($viewName, $viewData);
+                $this->loadViewInTemplate($viewName, $viewData);
                 ?>    
             </div>
             <div class="fixed-bottom bg-dark text-white">...</br>...</div>
-            
+
 
         </div>
         <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery-3.3.1.min.js"></script>
