b12a2e4e15f4e9d9aa32a4842648d99c4370d0fe
NOC 2 <NOC 2@DESKTOP-HR9FIKA>
2018-06-26 18:07:16 -0300

    Atualização Template

--- views/home.php
+++ views/home.php
@@ -3,27 +3,39 @@
         <div class="col d-flex align-items-center border-right border-dark">
             <nav class="d-none d-md-block" aria-label="breadcrumb">
                 <ol class="breadcrumb m-0 p-0 pl-1">
-                    <li class="breadcrumb-item d-sm-inline-block  " style="padding-left: 0px;">
-                        <a class="btn btn-link px-1 active" href="#">Dashboard</a>
+                    <li class="breadcrumb-item d-sm-inline-block active" style="padding-left: 0px;">
+                        <a class="btn btn-link px-1" href="#">Dashboard</a>
                     </li>
-                    <li class="breadcrumb-item d-sm-inline-block" style="padding-left: 0px;">
+                    <li class="breadcrumb-item " style="padding-left: 0px;">
                         <a class="btn btn-link px-1" href="#">Library</a>
                     </li>
-                    <li class="breadcrumb-item active" aria-current="page" style="padding-left: 0px;">
+                    <li class="breadcrumb-item " aria-current="page" style="padding-left: 0px;">
                         <a class="btn btn-link px-1 " href="#">Dashboard</a>
                     </li>
                 </ol>
             </nav>
             <nav class="d-md-none">
                 <div class="dropdown drop-ticket">
-                    <button class="btn btn-link dropdown-toggle" type="button" id="dropTicketMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button class="btn btn-link dropdown-toggle" type="button" id="FastMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Tickets Pendentes
                     </button>
-                    <div class="dropdown-menu" aria-labelledby="dropTicketMenu">
-                        <a class="dropdown-item fa-sm">Seus Tickets</a>
-                        <a class="dropdown-item fa-sm active">Tickets Pendentes</a>
-                        <a class="dropdown-item fa-sm">Seus Tickets</a>
-                        <a class="dropdown-item fa-sm">Seus Tickets</a>
+                    <div class="dropdown-menu fastmenu bg-light" aria-labelledby="FastMenu"> 
+                        <a class="dropdown-item d-flex justify-content-between align-items-center" href="#">
+                            <span class="drop-item">Seus Tickets</span>
+                            <span class="badge badge-blue badge-pill">1</span>
+                        </a>
+                        <a class="dropdown-item d-flex justify-content-between align-items-center active" href="#">
+                            <span class="drop-item">Seus Tickets</span>
+                            <span class="badge badge-blue badge-pill">1</span>
+                        </a>
+                        <a class="dropdown-item d-flex justify-content-between align-items-center" href="#">
+                            <span class="drop-item">Seus Tickets</span>
+                            <span class="badge badge-blue badge-pill">1</span>
+                        </a>
+                        <a class="dropdown-item d-flex justify-content-between align-items-center" href="#">
+                            <span class="drop-item">Seus Tickets</span>
+                            <span class="badge badge-blue badge-pill">1</span>
+                        </a>                        
                     </div>
                 </div>
             </nav>
@@ -32,8 +44,8 @@
             <form method="POST" class="search-form form-inline justify-content-end" role="form" style="width: 200px;">
                 <div class="form-group m-0 collapse width" id="SearchArea" aria-labelledby="SearchArea">
                     <div class="d-flex d-inline-block">
-                        <input type="text" class="form-control-sm mr-1 " name="fastSearch" placeholder="Pesquisar"/>
-                        <button type="submit" class="btn btn-sm btn-info">
+                        <input type="text" class="form-control form-control-sm mr-1 " name="fastSearch" placeholder="Pesquisar"/>
+                        <button type="submit" class="btn btn-sm btn-search">
                             <span class="fas fa-search"></span>
                         </button>
                     </div>
@@ -46,7 +58,10 @@
     </div>
     <div class="row tiled-area mt-3">
         <div class="col-lg-3 col-sm-6 mb-3">
-            <div class="dash-tile bg-danger d-flex align-items-center justify-content-center">
+            <div class="dash-tile dash-tile-blue d-flex align-items-center justify-content-center">
+                <div class="tile-icon">
+                    <i class="far fa-inbox-in"></i>
+                </div> 
                 <div class="tile-content pb-4">
                     <div class="tile-info">
                         0
@@ -58,7 +73,10 @@
             </div>
         </div>
         <div class="col-lg-3 col-sm-6 mb-3">
-           <div class="dash-tile bg-info d-flex align-items-center justify-content-center">
+           <div class="dash-tile dash-tile-red d-flex align-items-center justify-content-center">
+                <div class="tile-icon">
+                    <i class="far fa-inbox" style="font-size: 4.9rem; top: 0.8rem;"></i>
+                </div> 
                 <div class="tile-content pb-4">
                     <div class="tile-info">
                         0
@@ -70,19 +88,25 @@
             </div>
         </div>
         <div class="col-lg-3 col-sm-6 mb-3">
-            <div class="dash-tile bg-success d-flex align-items-center justify-content-center">
+            <div class="dash-tile dash-tile-violet d-flex align-items-center justify-content-center">
+                <div class="tile-icon">
+                    <i class="far fa-list-ul"></i>
+                </div> 
                 <div class="tile-content pb-4">
                     <div class="tile-info">
                         0
                     </div>
                     <span class="tile-text">
                         Tarefas Novas
-                    </span>
+                    </span>                                       
                 </div>                
             </div>
         </div>
         <div class="col-lg-3 col-sm-6 mb-3">
-            <div class="dash-tile bg-primary d-flex align-items-center justify-content-center">
+            <div class="dash-tile dash-tile-magenta d-flex align-items-center justify-content-center">
+                <div class="tile-icon">
+                    <i class="far fa-tasks"></i>
+                </div> 
                 <div class="tile-content pb-4">
                     <div class="tile-info">
                         0
@@ -95,8 +119,29 @@
         </div>
     </div>
     <div class="row ticket-area bg-secondary" >
-        <div class="col-md-3 d-md-block d-none">
-            ...
+        <div class="col-md-3 d-md-block d-none">            
+            <div class="fastmenu bg-light"> 
+                <span class="dropdown-header fa-lg">                    
+                    TICKETS
+                </span>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item d-flex justify-content-between align-items-center" href="#">
+                    <span class="drop-item">Seus Tickets</span>
+                    <span class="badge badge-blue badge-pill">1</span>
+                </a>
+                <a class="dropdown-item d-flex justify-content-between align-items-center active" href="#">
+                    <span class="drop-item">Seus Tickets</span>
+                    <span class="badge badge-blue badge-pill">1</span>
+                </a>
+                <a class="dropdown-item d-flex justify-content-between align-items-center" href="#">
+                    <span class="drop-item">Seus Tickets</span>
+                    <span class="badge badge-blue badge-pill">1</span>
+                </a>
+                <a class="dropdown-item d-flex justify-content-between align-items-center" href="#">
+                    <span class="drop-item">Seus Tickets</span>
+                    <span class="badge badge-blue badge-pill">1</span>
+                </a>                        
+            </div>
         </div>
         <div class="col-md bg-primary">
             ...
