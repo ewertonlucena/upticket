36dddae06010e07ad166f313a755a3a09ef81372
ewert <ewert@10.0.0.103>
2018-07-02 00:07:07 -0300

    Template 0.9 - Ticket Layout working...

--- views/tickets.php
+++ views/tickets.php
@@ -6,7 +6,7 @@
                     <thead>
                         <tr>
                             <th scope="col">Assunto</th>
-                            <th class="d-none d-lg-block" scope="col">Empresa</th>
+                            <th class="d-none d-lg-table-cell" scope="col">Empresa</th>
                             <th scope="col">Situação</th>
                             <th scope="col">Agente</th>
                             <th scope="col">Prioridade</th>
@@ -17,7 +17,7 @@
                     <tbody class="fa-sm">
                         <tr>
                             <td><span class="d-none d-md-inline-block">#3fa61c13 - </span> Baixa Performance IP Wirelink</td>
-                            <td class="d-none d-lg-block">Wirelink</td>
+                            <td class="d-none d-lg-table-cell">Wirelink</td>
                             <td>Aguardando Agente</td>
                             <td>Redes</td>
                             <td>Urgente</td>
@@ -26,7 +26,16 @@
                         </tr>
                         <tr>
                             <td>#3fa61c13 - Baixa Performance IP Wirelink</td>
-                            <td class="d-none d-lg-block">Wirelink</td>
+                            <td class="d-none d-lg-table-cell">Wirelink</td>
+                            <td>Aguardando Agente</td>
+                            <td>Rafael Galdino</td>
+                            <td>Urgente</td>
+                            <td>Faltam 2 dias</td>
+                            <td>Há 5 minutos</td>
+                        </tr>
+                        <tr>
+                            <td>#3fa61c13 - Baixa Performance IP Wirelink</td>
+                            <td class="d-none d-lg-table-cell">Wirelink</td>
                             <td>Aguardando Agente</td>
                             <td>Redes</td>
                             <td>Urgente</td>
@@ -35,16 +44,7 @@
                         </tr>
                         <tr>
                             <td>#3fa61c13 - Baixa Performance IP Wirelink</td>
-                            <td class="d-none d-lg-block">Wirelink</td>
-                            <td>Aguardando Agente</td>
-                            <td>Redes</td>
-                            <td>Urgente</td>
-                            <td>Faltam 2 dias</td>
-                            <td>Há 5 minutos</td>
-                        </tr>
-                        <tr>
-                            <td>#3fa61c13 - Baixa Performance IP Wirelink</td>
-                            <td class="d-none d-lg-block">Wirelink</td>
+                            <td class="d-none d-lg-table-cell">Wirelink</td>
                             <td>Aguardando Agente</td>
                             <td>Redes</td>
                             <td>Urgente</td>
