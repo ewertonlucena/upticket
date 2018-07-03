0cfca98129035d6564c285548d674f77dd05a731
ewert <ewert@10.0.0.103>
2018-07-03 08:19:26 -0300

    Template 0.9 - Ticket Layout working...

--- views/tickets.php
+++ views/tickets.php
@@ -1,53 +1,53 @@
 <div class="container-fluid px-0">
-    <section name="ticket-content">
+    <section class="d-none d-sm-block" name="tickets-table">
         <div class="row pt-3">
             <div class="col">
                 <table class="table table-sm table-hover">
                     <thead>
                         <tr>
                             <th scope="col">Assunto</th>
-                            <th class="d-none d-lg-table-cell" scope="col">Empresa</th>
+                            <th class="d-none d-xl-table-cell" scope="col">Empresa</th>
                             <th scope="col">Situação</th>
-                            <th scope="col">Agente</th>
-                            <th scope="col">Prioridade</th>
+                            <th scope="col">Responsável</th>
+                            <th class="d-none d-lg-table-cell" scope="col">Prioridade</th>
                             <th scope="col">SLA</th>
                             <th scope="col">Data</th>
                         </tr>
                     </thead>
                     <tbody class="fa-sm">
                         <tr>
-                            <td><span class="d-none d-md-inline-block">#3fa61c13 - </span> Baixa Performance IP Wirelink</td>
-                            <td class="d-none d-lg-table-cell">Wirelink</td>
+                            <td><span class="d-none d-lg-inline-block">#3fa61c13 - </span> Baixa Performance IP Wirelink</td>
+                            <td class="d-none d-xl-table-cell">Wirelink</td>
                             <td>Aguardando Agente</td>
                             <td>Redes</td>
-                            <td>Urgente</td>
+                            <td class="d-none d-lg-table-cell">Urgente</td>
                             <td>Faltam 2 dias</td>
                             <td>Há 5 minutos</td>
                         </tr>
                         <tr>
-                            <td>#3fa61c13 - Baixa Performance IP Wirelink</td>
-                            <td class="d-none d-lg-table-cell">Wirelink</td>
+                            <td><span class="d-none d-lg-inline-block">#3fa61c13 - </span> Baixa Performance IP Wirelink</td>
+                            <td class="d-none d-xl-table-cell">Wirelink</td>
                             <td>Aguardando Agente</td>
                             <td>Rafael Galdino</td>
-                            <td>Urgente</td>
+                            <td class="d-none d-lg-table-cell">Urgente</td>
                             <td>Faltam 2 dias</td>
                             <td>Há 5 minutos</td>
                         </tr>
                         <tr>
-                            <td>#3fa61c13 - Baixa Performance IP Wirelink</td>
-                            <td class="d-none d-lg-table-cell">Wirelink</td>
+                            <td><span class="d-none d-lg-inline-block">#3fa61c13 - </span> Baixa Performance IP Wirelink</td>
+                            <td class="d-none d-xl-table-cell">Wirelink</td>
                             <td>Aguardando Agente</td>
                             <td>Redes</td>
-                            <td>Urgente</td>
+                            <td class="d-none d-lg-table-cell">Urgente</td>
                             <td>Faltam 2 dias</td>
                             <td>Há 5 minutos</td>
                         </tr>
                         <tr>
-                            <td>#3fa61c13 - Baixa Performance IP Wirelink</td>
-                            <td class="d-none d-lg-table-cell">Wirelink</td>
+                            <td><span class="d-none d-lg-inline-block">#3fa61c13 - </span> Baixa Performance IP Wirelink</td>
+                            <td class="d-none d-xl-table-cell">Wirelink</td>
                             <td>Aguardando Agente</td>
                             <td>Redes</td>
-                            <td>Urgente</td>
+                            <td class="d-none d-lg-table-cell">Urgente</td>
                             <td>Faltam 2 dias</td>
                             <td>Há 5 minutos</td>
                         </tr> 
@@ -55,6 +55,101 @@
                 </table>
             </div>
         </div>
+    </section>
+    <section class="d-sm-none" name="tickets-cards">
+        <div class="card recent-ticket" >
+                <div class="card-header border-bottom-0">
+                    Tickets Recentes
+                </div>
+                <div class="card-body d-none">
+                    <div class="card-text">
+                        Não existem tickets recentes.
+                    </div>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <a class="link-card" href="#">
+                            <div class="d-flex d-inline-block align-items-center justify-content-between">
+                                <span class="text-truncate">                                
+                                    <span class="d-none d-sm-inline-block">#3fa61c13 -</span> Baixa Performance - IP Wirelink
+                                </span> 
+                                <span class="badge badge-yellow text-nowrap ml-1">
+                                    respondido
+                                </span>
+                            </div>                            
+                            <div class="d-flex d-inline-block align-items-center justify-content-between">
+                                <small>Wirelink</small>
+                                <small>Há 2 hora(s)</small>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="list-group-item">
+                        <a class="link-card" href="#">
+                            <div class="d-flex d-inline-block align-items-center justify-content-between">
+                                <div class="text-truncate">                                
+                                    <span class="d-none d-sm-inline-block">#3fa61c13 -</span> Baixa Performance - IP Wirelink
+                                </div> 
+                                <span class="badge badge-orange text-nowrap ml-1">
+                                    pendente
+                                </span>
+                            </div>                            
+                            <div class="d-flex d-inline-block align-items-center justify-content-between">
+                                <small>Wirelink</small>
+                                <small>Há 2 hora(s)</small>
+                            </div>
+                        </a>
+                    </li>
+                    
+                    <li class="list-group-item">
+                        <a class="link-card" href="#">
+                            <div class="d-flex d-inline-block align-items-center justify-content-between">
+                                <div class="text-truncate">                                
+                                    <span class="d-none d-sm-inline-block">#3fa61c13 -</span> Baixa Performance - IP Wirelink
+                                </div> 
+                                <span class="badge badge-secondary text-nowrap ml-1">
+                                    fechado
+                                </span>
+                            </div>                            
+                            <div class="d-flex d-inline-block align-items-center justify-content-between">
+                                <small>Wirelink</small>
+                                <small>Há 2 hora(s)</small>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="list-group-item">
+                        <a class="link-card" href="#">
+                            <div class="d-flex d-inline-block align-items-center justify-content-between">
+                                <div class="text-truncate">                                
+                                    <span class="d-none d-sm-inline-block">#3fa61c13 -</span> Baixa Performance - IP Wirelink
+                                </div> 
+                                <span class="badge badge-success text-nowrap ml-1">
+                                    resolvido
+                                </span>
+                            </div>                            
+                            <div class="d-flex d-inline-block align-items-center justify-content-between">
+                                <small>Wirelink</small>
+                                <small>Há 2 hora(s)</small>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="list-group-item">
+                        <a class="link-card" href="#">
+                            <div class="d-flex d-inline-block align-items-center justify-content-between">
+                                <div class="text-truncate">                                
+                                    <span class="d-none d-sm-inline-block">#3fa61c13 -</span> Baixa Performance - IP Wirelink
+                                </div> 
+                                <span class="badge badge-red text-nowrap ml-1">
+                                    vencido                                    
+                                </span>
+                            </div>                            
+                            <div class="d-flex d-inline-block align-items-center justify-content-between">
+                                <small>Wirelink</small>
+                                <small>Há 2 hora(s)</small>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>        
     </section>
 </div>
 
