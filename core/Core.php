aa17c8b110d79e9bb32beb75dbd14e81cff985fc
NOC 2 <NOC 2@DESKTOP-HR9FIKA>
2018-07-07 17:14:48 -0300

    Template 1.0 Agent Panel Layout 0.91 Admin Panel - Agents Area - Initialized

--- core/Core.php
+++ core/Core.php
@@ -1,44 +1,44 @@
 <?php
+
 class Core {
 
-        public function run(){
-                
-                $url= isset($_GET['url'])?'/'.$_GET['url']:'/';                          
-		$params = array();
-		if(!empty($url) && $url != '/') {
-			$url = explode('/', $url);                       
-			array_shift($url); 
-                        
-                        if($url[0] == 'admin') {
-                            array_shift($url);
-                            $currentController = 'panelController';
-                        }
-                        
-                        if(isset($url[0]) && !empty($url[0])) {
-                            $currentController = $url[0].'Controller';                         
-                        }
-                        
-                        array_shift($url);
-                        
-			if(isset($url[0]) && !empty($url[0])) {                           
-				$currentAction = $url[0];
-                                array_shift($url);
-			} else {
-				$currentAction = 'index';
-			}
-			if(!empty($url[0])) {                            
-				$params = $url;                                
-                                if(empty(end($params))){                                    
-                                    array_pop($params);
-                                }                                
-			}
-		} else {
-			$currentController = 'homeController';
-			$currentAction = 'index';
-		}                
-		$c = new $currentController();
-		call_user_func_array(array($c, $currentAction), $params);	
+    public function run() {
+
+        $url = isset($_GET['url']) ? '/' . $_GET['url'] : '/';
+        $params = array();
+        if (!empty($url) && $url != '/') {
+            $url = explode('/', $url);
+            array_shift($url);
+
+            if ($url[0] == 'admin') {
+                array_shift($url);
+                $currentController = 'panelController';
+            }
+
+            if (isset($url[0]) && !empty($url[0])) {
+                $currentController = $url[0] . 'Controller';
+            }
+
+            array_shift($url);
+
+            if (isset($url[0]) && !empty($url[0])) {
+                $currentAction = $url[0];
+                array_shift($url);
+            } else {
+                $currentAction = 'index';
+            }
+            if (!empty($url[0])) {
+                $params = $url;
+                if (empty(end($params))) {
+                    array_pop($params);
+                }
+            }
+        } else {
+            $currentController = 'homeController';
+            $currentAction = 'index';
         }
+        $c = new $currentController();
+        call_user_func_array(array($c, $currentAction), $params);
+    }
+
 }
-         
-        
